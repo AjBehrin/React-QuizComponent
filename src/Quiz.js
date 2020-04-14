ebc3f65fdf6c88c0c62dbf03c14e4e41fdd62f64
Alex Behringer <Ajbehrin@gmail.com>
2020-04-14 09:59:15 -0500

    Module 5 Work
    Completed module 5 with conditional rendering logic, may flag as wrong even though it is currently working

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -11,9 +12,28 @@ class Quiz extends React.Component {
     }
 
     render() {
+
+        const isQuizEnd = () => {
+            if ((this.state.quiz_position - 1) === quizData.quiz_questions.length) {
+                return true;
+            } else {
+                return false; 
+            }
+        }
+
+        let checkQuiz = isQuizEnd();
+
+        const quizDisplay = (check) => {
+            if (check === true) {
+                return <QuizEnd />;
+            } else if (check === false) {
+                return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />;
+            }
+        }
+
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                {quizDisplay(checkQuiz)}
             </div>
         )
     }
